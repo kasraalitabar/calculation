47b8a93b6f72b2e7f54cc430d80d8fd0d0350ac2
kasraalitabar <kasraalitabar@gmail.com>
2025-02-08 17:27:20 +0330

    first

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const display = document.getElementById("display");
+const number = document.getElementById("btn");
+const equal =document.getElementById("equal");
+const clean =document.getElementById("clean");
+const amal =document.getElementById("amal");
+
+
